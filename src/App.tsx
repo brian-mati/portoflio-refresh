236a1b59a9753730b06414975e360fbc4a9fdbd1
mati-brian <brianmatiX@proton.me>
2025-10-24 16:01:17 +0300

    removed unused usestate hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+
 import './App.css'
 import './index.css'
 import Sidebar from './sidebar.tsx'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
