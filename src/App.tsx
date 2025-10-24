353cc8c8c056314933258a0a37e555596bfcae45
mati-brian <brianmatiX@proton.me>
2025-10-24 14:58:30 +0300

    setup tailwind css and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
 import { useState } from 'react'
 import './App.css'
+import './index.css'
+import Sidebar from './sidebar.tsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-         <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-
-        </button>
-        </div>
-        <h1> Hello world test </h1> 
+      <main>
+                          <Sidebar/>
+        <section className='max-w-md leading-7'>
+          <p> Hello there and welcome to my small corner
+of the inter webs where i share my experiences,
+code and designs.  
+<span className='inline-block mt-8'>
+I started my journey into to programming and
+computer science in general 2023.  I am purely
+self taught and all the skills acquired have been 
+through grueling ours of building applications 
+to entertain and test myself. <span className='inline-block mr-4 underline'>Read more</span>
+ </span>
+ </p>
+        </section>
+      </main>
        </>
   )
 }
---
+++ src/sidebar.tsx
@@ -0,0 +1,79 @@
+import { useEffect, useRef } from 'react';
+import { Player } from '@lordicon/react';
+import home from './assets/home.json';
+import about from './assets/about.json';
+import resume from './assets/resume.json';
+import schedule from './assets/schedule.json';
+
+
+
+function Home(){
+  const playerRef = useRef<Player>(null);
+  useEffect(() => {
+    playerRef.current?.playFromBeginning()
+  },[])
+  return(
+    <Player
+    ref={playerRef}
+    icon={home}
+    onComplete={() => playerRef.current?.playFromBeginning()}
+    />
+  )
+}
+
+function About(){
+  const playerRef = useRef<Player>(null);
+  useEffect(() => {
+    playerRef.current?.playFromBeginning()
+  },[])
+  return(
+    <Player
+    ref={playerRef}
+    icon={about}
+    onComplete={() => playerRef.current?.playFromBeginning()}
+    />
+  )
+}
+
+function Resume(){
+  const playerRef = useRef<Player>(null);
+  useEffect(() => {
+    playerRef.current?.playFromBeginning()
+  },[])
+  return(
+    <Player
+    ref={playerRef}
+    icon={resume}
+    onComplete={() => playerRef.current?.playFromBeginning()}
+    />
+  )
+}
+
+function Schedule(){
+  const playerRef = useRef<Player>(null);
+  useEffect(() => {
+    playerRef.current?.playFromBeginning()
+  },[])
+  return(
+    <Player
+    ref={playerRef}
+    icon={schedule}
+    onComplete={() => playerRef.current?.playFromBeginning()}
+    />
+  )
+}
+
+
+export default function Sidebar() {
+  return (
+    <nav className='max-h-sm self-center'>
+      <ul className='flex flex-col gap-4'>
+        <li><Home/></li>
+        <li><About/></li>
+        <li><Resume/></li>
+        <li><Schedule/></li>
+      </ul>
+    </nav>
+  );
+}
+
