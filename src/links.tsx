1329c69114bc66d61cbf7fbf6c204819fa478d9e
mati-brian <brianmatiX@proton.me>
2025-10-25 13:28:23 +0300

    removed unused in link.tsx

--- src/links.tsx
+++ src/links.tsx
@@ -1,27 +1,10 @@
-import { useEffect, useRef, useState } from "react";
-import { Player } from '@lordicon/react';
-import email from './assets/email.json';
 
-
-function Email(){
-    const [isHovered,setIsHovered] = useState(false)
-    const playRef = useRef<Player>(null);
-    useEffect( () => {
-        playRef.current?.playFromBeginning()
-    } ,[] )
-
-    return(
-        <button className="inline">
-
-        </button>
-    )
-}
 
 export default function Links(){
   
     return(
         <ul className="links">
-            <li></li>
+            <li>Link position</li>
         </ul>
     )
 }
